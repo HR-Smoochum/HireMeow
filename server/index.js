e04c19b4e755a49a1cadadb76ad5d4d3f062ef19
Patrick Kelly <patrick.will.kelly@gmail.com>
2023-03-03 15:52:32 -0800

    complete initial directory structure of main repo branch

---
+++ server/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const app = express();
+const router = require('./router');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/test', (req, res) => {
+    res.send('hello jobsite!')
+});
+app.use('/', express.static(path.join(__dirname, '../client/dist')));
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+})
