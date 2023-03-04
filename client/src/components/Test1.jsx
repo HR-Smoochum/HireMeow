019382d0d23bf089b3457f1097adfc2711ff0b89
Kathy Ye <kathyye15@gmail.com>
2023-03-04 15:27:05 -0800

    implemeted color palatte with chakra

---
+++ client/src/components/Test1.jsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { Stack, Box, Center, Checkbox, CheckboxGroup } from '@chakra-ui/react'
+
+export default function TestDiv() {
+  return (
+    <Center w='100px' h='100px'>
+      <Box bg='brand.yellow' w='100%' p={4} color='brand.teal'>
+        This is the Box
+        <Stack spacing={5} direction='row'>
+          <Checkbox isDisabled>Checkbox</Checkbox>
+          <Checkbox isDisabled defaultChecked>
+            Checkbox
+          </Checkbox>
+        </Stack>
+      </Box>
+    </Center>
+  );
+};
+
