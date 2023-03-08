e494c05affa0bb4096678f0795d646105f820989
Donna <donnatopia@gmail.com>
2023-03-08 14:40:48 -0800

    create navigation to the signup, login, and logout

--- client/src/components/Header/Options.jsx
+++ client/src/components/Header/Options.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useNavigate } from 'react';
 import PropTypes from 'prop-types';
 import { MenuList, MenuItem, MenuDivider } from '@chakra-ui/react';
+import { useAuth } from '../Auth/contexts/AuthContext.jsx';
 
 export default function Options({ mode, firstName }) {
+  const { logout } = useAuth();
+  const navigate = useNavigate();
   const salutation = `Welcome, ${firstName}!`;
+
+  const handleNavigate = (e, path) => {
+    e.preventDefault();
+    navigate(path);
+  };
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    logout()
+      .then(() => navigate('/'))
+      .catch((err) => console.log(err));
+  };
 
   if (mode !== 'guest') {
     return (
       <MenuList>
         <MenuItem>{ salutation }</MenuItem>
         <MenuDivider />
-        <MenuItem>Log Out</MenuItem>
+        <MenuItem onClick={(e) => handleLogout(e)}>Log Out</MenuItem>
       </MenuList>
     );
   }
@@ -19,8 +34,8 @@ export default function Options({ mode, firstName }) {
     <MenuList>
       <MenuItem>{ salutation }</MenuItem>
       <MenuDivider />
-      <MenuItem>Create an Account</MenuItem>
-      <MenuItem>Log In</MenuItem>
+      <MenuItem onClick={(e) => handleNavigate(e, '/signup')}>Create an Account</MenuItem>
+      <MenuItem onClick={(e) => handleNavigate(e, '/login')}>Log In</MenuItem>
     </MenuList>
   );
 }
