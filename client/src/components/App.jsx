c45874309dd9e4937c5af9f9d0a7987b6eab5baa
Donna <donnatopia@gmail.com>
2023-03-08 10:32:20 -0800

    fixed the routes for the my jobs section and main page

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,19 +1,18 @@
 // LIBRARY IMPORTS
 import React from 'react';
-import { extendTheme, ChakraProvider, Container } from '@chakra-ui/react';
+import { extendTheme, ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './Auth/contexts/AuthContext';
 // import PrivateRoute from './Auth/PrivateRoute';
 
 // LOCAL IMPORTS
-import Header from './Header/Header.jsx';
-import Login from './Auth/Login.jsx';
-import Signup from './Auth/Signup.jsx';
 import SearchPage from './SearchPage/SearchPage.jsx';
 import Notes from './Notes/Notes.jsx';
 import Calendar from './Calendar/Calendar.jsx';
 import Blog from './Blog/Blog.jsx';
 import Resume from './Resume/Resume.jsx';
+import SavedJobs from './MyJobPostings/CardDashboard';
+import SavedSeekers from './MyJobPostings/JobSeekerCard';
 
 // COMPONENT
 function App() {
@@ -40,16 +39,13 @@ function App() {
       <Router>
         <AuthProvider>
           <Routes>
-            {/* Default path is to the header => will be removed when everyone adds headers to their components */}
-            <Route exact path="/" element={<Login />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/jobs" element={<SearchPage />} />
+            <Route exact path="/" element={<SearchPage />} />
+            <Route path="/jobs" element={<SavedJobs />} />
+            <Route path="/seekers" element={<SavedSeekers />} />
             <Route path="/notes" element={<Notes />} />
             <Route path="/calendar" element={<Calendar />} />
             <Route path="/blog" element={<Blog />} />
             <Route path="/resume" element={<Resume />} />
-            <Route path="/seekers" element={<SearchPage />} />
           </Routes>
         </AuthProvider>
       </Router>
