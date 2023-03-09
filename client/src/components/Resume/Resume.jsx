1e2251ebbabe2ba5a82eb3236fb7efa5da4283a1
Patrick Kelly <patrick.will.kelly@gmail.com>
2023-03-09 14:21:19 -0800

    implement save resume functionality

--- client/src/components/Resume/Resume.jsx
+++ client/src/components/Resume/Resume.jsx
@@ -1,16 +1,62 @@
 // LIBRARY IMPORTS
-import React from 'react';
-
+import React, { useState } from 'react';
+import Axios from 'axios';
 // LOCAL IMPORTS
+import Header from '../Header/Header.jsx';
+
+const FormData = require('form-data');
 
 // COMPONENT
 function Resume() {
+  const [selectedFile, setSelectedFile] = useState('');
+  const [fileName, setFileName] = useState('');
+  const [displayName, setDisplayName] = useState('');
+  const [resumeImage, setResumeImage] = useState();
+  const [resumePresent, setResumePresent] = useState(false);
+
+  const handleFileUpload = (e) => {
+    setSelectedFile(e.target.files[0]);
+    setFileName(e.target.files[0].name);
+  };
+
+  const submitForm = () => {
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+    formData.append('name', fileName);
+
+    Axios.post('/resumeData', formData, {
+      headers: {
+        'content-type': 'multipart/form-data',
+      },
+    })
+      .then((res) => {
+        console.log('upload successful ', res);
+      })
+      .catch((err) => {
+        console.log('upload failed ', err);
+      });
+
+    Axios.get('/getResume')
+      .then((res) => {
+        setResumePresent(!resumePresent);
+      });
+  };
+
   return (
     <div className="uploadResumeContainer">
-      <div className="myResumeHeader" />
-      <div className="uploadResumeActionContainer">
-        <div className="uploadResume">Upload Resume</div>
-        <button className="uploadButton">Upload</button>
+      <Header />
+      <div className="myResumeHeader">Resume</div>
+      <div className="resumeInformationContainer">
+        <div className="uploadResumeActionContainer">
+          <div className="uploadResume">Upload Resume</div>
+          <input className="inputResume" type="file" name="uploadResume" accept=".doc,.docx,.pdf" onChange={handleFileUpload} />
+          <button type="submit" className="uploadResumeButton" onClick={submitForm}>Upload!</button>
+        </div>
+        <div className="resumeImageContainer">
+          {
+          resumePresent ? <iframe className="resumeImage" title="resumeImage" src="http://localhost:3000/getResume" /> : <h1 className="noResume">No Resume Available</h1>
+        }
+        </div>
       </div>
     </div>
   );
