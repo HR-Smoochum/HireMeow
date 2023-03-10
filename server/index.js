19c945cb99e609a4ec95d64f64f3ad2a7490eed2
Patrick Kelly <patrick.will.kelly@gmail.com>
2023-03-09 16:17:56 -0800

    pull down update from main

--- server/index.js
+++ server/index.js
@@ -19,11 +19,6 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/'));
 });
 
-// serves the static files after url has been refreshed
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/'));
-});
-
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
