9942b571809b5822fc81f0df0647596965b64d08
Daniel Greaves <dgreaves18@gmail.com>
2023-03-09 18:03:29 -0800

    Sign up now posts user to corresponding db table

--- client/src/components/Auth/Signup.jsx
+++ client/src/components/Auth/Signup.jsx
@@ -2,19 +2,20 @@
 // LIBRARY IMPORTS
 import React, { useState } from 'react';
 import {
-  FormControl, FormLabel, Input, Card, CardBody, Button, Alert, Radio, RadioGroup, Stack, Heading, Center,
+  FormControl, FormLabel, FormHelperText, Input, Card, CardBody, Button, Alert, Radio, RadioGroup, Stack, Heading, Center,
 } from '@chakra-ui/react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useAuth } from './contexts/AuthContext';
-import Header from '../Header/Header.jsx';
+import axios from 'axios';
 
 // LOCAL IMPORTS
+import { useAuth } from './contexts/AuthContext';
+import Header from '../Header/Header.jsx';
 
 // COMPONENT
 function Signup() {
   const [email, setEmail] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
+  const [first, setFirst] = useState('');
+  const [last, setLast] = useState('');
   const [industry, setIndustry] = useState('');
   const [password, setPassword] = useState('');
   const [passwordConfirm, setPasswordConfirm] = useState('');
@@ -37,9 +38,25 @@ function Signup() {
       setLoading(true);
       await signup(email, password);
     } catch {
-      setError('Failed to create account');
+      setLoading(false);
+      return setError('Failed to create account');
     }
     setLoading(false);
+    // post user object to db
+    if (currentUser) {
+      const { uid } = currentUser;
+      const userData = {
+        uid,
+        first_name: first,
+        last_name: last,
+        industry,
+      };
+      if (user === 'seeker') {
+        axios.post('/seekers/signup', userData);
+      } else if (user === 'employer') {
+        axios.post('/employers/signup', userData);
+      }
+    }
     // console.log(currentUser.uid);
     navigate('/login');
   }
@@ -58,15 +75,15 @@ function Signup() {
           <form onSubmit={handleSubmit}>
             <FormControl>
               <FormLabel>Email</FormLabel>
-              <Input type="email" id="1" value={email} onChange={(e) => setEmail(e.target.value)} required />
+              <Input type="email" id="1" value={email} placeholder="example@example.com" onChange={(e) => setEmail(e.target.value)} required />
               <FormLabel>First Name</FormLabel>
-              <Input type="text" id="2" value={firstName} onChange={(e) => setFirstName(e.target.value)} required />
+              <Input type="text" id="2" value={first} onChange={(e) => setFirst(e.target.value)} required />
               <FormLabel>Last Name</FormLabel>
-              <Input type="text" id="3" value={lastName} onChange={(e) => setLastName(e.target.value)} required />
+              <Input type="text" id="3" value={last} onChange={(e) => setLast(e.target.value)} required />
               <FormLabel>Industry</FormLabel>
-              <Input type="text" id="4" value={industry} onChange={(e) => setIndustry(e.target.value)} required />
+              <Input type="text" id="4" value={industry} placeholder="Ex: Health & Wellness" onChange={(e) => setIndustry(e.target.value)} required />
               <FormLabel>Password</FormLabel>
-              <Input type="password" id="5" value={password} onChange={(e) => setPassword(e.target.value)} required />
+              <Input type="password" id="5" pattern=".{6,14}" placeholder="Password must be 6-14 characters" value={password} onChange={(e) => setPassword(e.target.value)} required />
               <FormLabel>Confirm Password</FormLabel>
               <Input type="password" id="6" value={passwordConfirm} onChange={(e) => setPasswordConfirm(e.target.value)} required />
               <Center p={4}>
